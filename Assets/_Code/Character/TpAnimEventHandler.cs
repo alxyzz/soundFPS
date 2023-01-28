f7d075d9c3110d4ba7898412ae7a9976e96c8b26
alxyzz <93402865+alxyzz@users.noreply.github.com>
2023-01-28 21:15:04 +0100

    netcode, reworked joining + a bit of reorganization [WIP]

---
+++ Assets/_Code/Character/TpAnimEventHandler.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TpAnimEventHandler : CharacterAnimEventHandler
+{
+    //private AudioSource _audioSource;
+    //private unused_CharacterMovement _charaMovement;
+    //protected override void Awake()
+    //{
+    //    base.Awake();
+    //    _audioSource = GetComponent<AudioSource>();
+    //    _charaMovement = GetComponentInParent<unused_CharacterMovement>();
+    //}
+
+    //private void FootStep()
+    //{
+    //    if (!_charaMovement.IsWalking)            
+    //    {
+    //        _audioSource.PlayOneShot(SoundList.GetRandomFootstep());
+    //    }
+    //    // GetComponentInParent<LocalPlayerController>().NotifyServerOfFootstep();
+    //}
+}
